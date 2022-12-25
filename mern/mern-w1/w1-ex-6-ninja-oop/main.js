b60eed2430d1ab098fe1f501ac410257cf5a793a
Francisco Sosa <frmsos@MacBook-Pro-de-Francisco.local>
2022-12-25 15:26:25 -0300

    w1-ex-6-oop

---
+++ mern/mern-w1/w1-ex-6-ninja-oop/main.js
@@ -0,0 +1,25 @@
+//creamos la clase ninja
+class ninja{
+    //atributos
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3; // valor predeterminado
+        this.strength = 3; //valor predeterminado
+    }
+    //metodos
+    sayName() {
+        console.log("El nombre del Ninja es:  ", this.name);
+    }
+    showStats(){
+        console.log("Ninja ", this.name, " \n", "Fuerza: ", this.strength, "\n", "Velocidad: ", this.speed, "\n", "Salud: ", this.health);
+    }
+    drinkSake(){
+        this.speed +=10;
+    }
+}
+//probamos
+ninja2 = new ninja('peter', 10);
+ninja2.showStats();
+ninja2.drinkSake();
+ninja2.showStats();
