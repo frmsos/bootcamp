958c3abdfb564c490b02fb24192bfdc8954ffb44
Francisco Sosa <frmsos@MacBook-Pro-de-Francisco.local>
2023-01-02 17:30:50 -0300

    w2-ex-2-core-finished

---
+++ mern/mern-w2/w2-ex-2-core-first-react-jsx/first-react-jsx/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="FirstApp">
+      <header className="firstReactWithJSX">
+        <h1>Hello Dojo!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
