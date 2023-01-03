ecc99636ead7b50657d777ae014c2a2c519e03eb
Francisco Sosa <frmsos@MacBook-Pro-de-Francisco.local>
2023-01-02 21:28:46 -0300

    w2-ex4-core done

---
+++ mern/mern-w2/w2-ex-4-core-todojunto/apoyarlo/src/components/PersonCard.js
@@ -0,0 +1,30 @@
+import {Component} from "react";
+import React from "react";
+
+class PersonCard extends Component{
+   constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+   }
+   increaseAge = () => {
+        let ageCounter = this.state.age;
+        ageCounter++;
+        this.setState( { age: ageCounter } );
+        console.log(ageCounter);
+   }
+   
+    render(){
+        return(
+            <div className="list">
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.haircolor}</p>
+                <button onClick = { this.increaseAge   }> Birthday button for {this.props.firstName} {this.props.lastName} </button>
+            </div>
+            
+        );
+    }
+}
+export default PersonCard;
