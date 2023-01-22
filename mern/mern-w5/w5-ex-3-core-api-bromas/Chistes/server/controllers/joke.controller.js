4f8488492510f5c4dfa061b2be9372aeb51afa24
Francisco Sosa <frmsos@MacBook-Pro-de-Francisco.local>
2023-01-22 10:17:27 -0300

    done joke api with random values

--- mern/mern-w5/w5-ex-3-core-api-bromas/Chistes/server/controllers/joke.controller.js
+++ mern/mern-w5/w5-ex-3-core-api-bromas/Chistes/server/controllers/joke.controller.js
@@ -2,8 +2,9 @@ const Joke = require("../models/joke.model");
 
 module.exports = {
   findAllJokes : (req, res) => {
+    console.log('miausa');
     Joke.find()
-      .then(allJokes => res.json({ jokes: allJokes }))
+      .then(allJokes => res.json( {jokes : allJokes} ))
       .catch(err => res.json({ message: "Error in find all jokes", error: err }))
   },
   findOneJoke : (req, res) => {
@@ -25,6 +26,15 @@ module.exports = {
     Joke.deleteOne({ _id: req.params.id })
       .then(result => res.json({ result: result }))
       .catch(err => res.json({ message: "Error deleting the joke", error: err }));
+  },
+  random : (req, res) => {
+    Joke.estimatedDocumentCount()
+    .then(count => (
+        Joke.findOne().skip(Math.floor(Math.random() * count))   //ref: https://stackoverflow.com/questions/39277670/how-to-find-random-record-in-mongoose
+        .then( joke => res.json( {joke} )  )
+        .catch( err => res.json (  { message: "Error in randomly selecting a joke", error: err }))
+    ))
+    .catch( err => res.json (  { message: "Error in reading the document count", error: err }))
   }
 
 };
--- mern/mern-w5/w5-ex-3-core-api-bromas/Chistes/server/routes/joke.routes.js
+++ mern/mern-w5/w5-ex-3-core-api-bromas/Chistes/server/routes/joke.routes.js
@@ -2,8 +2,10 @@ const Controller = require("../controllers/joke.controller");
 
 module.exports = app => {
   app.get("/api/jokes/", Controller.findAllJokes);
+  app.get("/api/jokes/random", Controller.random);
   app.get("/api/jokes/:id", Controller.findOneJoke);
   app.put("/api/jokes/update/:id", Controller.updateExistingJoke);
   app.post("/api/jokes/new", Controller.createNewJoke);
   app.delete("/api/jokes/delete/:id", Controller.deleteJoke);
+
 };
