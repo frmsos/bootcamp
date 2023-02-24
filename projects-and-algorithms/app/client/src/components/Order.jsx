b669460025b5c7bb28a54c674e7bdcdc166cc01c
frmsos <frmsos@gmail.com>
2023-02-23 21:56:56 -0300

    order page: done til modified values, still missing mod values show in textfield and show subtotal cost jue23feb2023 2158

--- projects-and-algorithms/app/client/src/components/Order.jsx
+++ projects-and-algorithms/app/client/src/components/Order.jsx
@@ -23,22 +23,38 @@ const Order = (props) => {
     const {register, handleSubmit, formState: {errors}} = useForm();
     const [order, setOrder] = useState({type: "", items: []});
     //const [typeValue, setTypeValue] = useState(method[0].label);
-    //const [toppingsOrder, setToppingsOrder] = useState([]);
-    let showButton ;
+    const [opMode, setOpMode] = useState("");
+    let showButton;
+    const [orderType, setOrderType] = useState("");
 
 
     //FUNCTIONS DECLARATIONS
-    const onSubmit = (data, pizzatopping) => {
+    const onSubmit = (data, pizzatopping, operation) => {
         const item = {
             topping : pizzatopping,
             size : data.size,
             crust: data.crust,
             amount : data.amount
         }
+        let modifiedVector;
+        if( operation === 'add')
+        {
+            setOrder( {...order, items: [...order.items, item]} );
+            //setOpMode(null);
+        }
+        else{
+            console.log('cjec', order.items)
+            const [isThereBool, indexFound] = isThere(pizzatopping, order.items, "boolIndex");
+            console.log(isThereBool, indexFound);
+            modifiedVector = order.items;
+            modifiedVector[indexFound] = item;
+            setOrder({...order, items: modifiedVector})
+        }
+
         //is there para ver si ya esta,
 
         //si no, esto
-        setOrder( {...order, items: [...order.items, item]} );
+        
 
         // si ya esta, hay que actualizar
         //con filter un nueva vector
@@ -49,20 +65,27 @@ const Order = (props) => {
       //  console.log("prueba print", order.items[0].topping)
 
     };
-    const isThere = (pizzatopping, requestItem) =>{
+    const isThere = (pizzatopping, requestItem, operation) =>{
         //alert(pizzatopping)
         for(let i = 0; i < requestItem.length; i++)
         {
             if(requestItem[i].topping === pizzatopping)
                 
                 {
-                    //alert('is here');
-                    return true;
+                    if( operation === 'bool' )
+                    {                        
+                        return true;
+                    }
+                    else
+                        return [true, i]
                 }
 
         }
         //alert('is not here')
-        return false;
+        if( operation === 'bool' )
+            return false;
+        else
+            return [false, null]
     }
     const handleToppingSelect = (event, pizzatopping) =>{
         event.preventDefault();
@@ -93,7 +116,7 @@ const Order = (props) => {
                                 <Autocomplete
                                     disablePortal
                                     id="combo-box-demo"
-                                    //onChange = {(e, value)=> alert(value) }
+                                    onChange = {(e, value)=> setOrderType(value) }
                                     options={method}
                                     isOptionEqualToValue = { (option, value) => option.value === value.value}
                                     sx={{ width: 300, p: 2 }}
@@ -106,7 +129,7 @@ const Order = (props) => {
                                 {  props.requestItem.map(  (pizzatopping,index) => {
                                     name = pizzatopping.charAt(0).toUpperCase() + pizzatopping.slice(1);  
                                     return (
-                                        <Box component="form" noValidate onSubmit={ handleSubmit( data => onSubmit(data, pizzatopping))}>
+                                        <Box component="form" noValidate onSubmit={ handleSubmit( data => onSubmit(data, pizzatopping, opMode))}>
                                             <Grid container item xs={12} key={index} spacing={3} sx={{p:3}} >
                                                 <Card sx={{ maxWidth: 500, p:2 }}>
                                                     <CardMedia
@@ -158,15 +181,28 @@ const Order = (props) => {
                                                         </IconButton>
                                                     </CardActions>
                                                             </Card>
-                                                    {showButton = isThere(pizzatopping, order.items)}
-                                                        {!showButton ? <Button
+                                                    {showButton = isThere(pizzatopping, order.items, 'bool')}
+                                                        {!(showButton) ? 
+                                                        <Button
+                                                            type="submit"
+                                                            fullWidth
+                                                            variant="contained"
+                                                            onClick={e=> setOpMode("add")}
+                                                            sx={{ mt: 3, mb: 2, ml: 1, bgcolor : "#008C45", width: "50%" }}
+                                                            >
+                                                            Agregar al carrito
+                                                        </Button> 
+                                                    : 
+                                                    <Button
                                                         type="submit"
                                                         fullWidth
                                                         variant="contained"
-                                                        sx={{ mt: 3, mb: 2, ml: 1, bgcolor : "#008C45", width: "50%" }}
+                                                        onClick={e=> setOpMode("modify")}
+                                                        sx={{ mt: 3, mb: 2, ml: 1, bgcolor : "#ffa500", width: "50%" }}
                                                         >
-                                                        Agregar al carrito
-                                                    </Button> : null}
+                                                        Modificar
+                                                    </Button> 
+                                                    }
                                                 </Grid>
                                             </Box>
                                         
@@ -174,6 +210,25 @@ const Order = (props) => {
                             </div>
                     <div className='containerSides'>
                         <Typography variant='h3' sx={{fontWeight:'bold', m:2} }> Historial de Pedidos </Typography>
+                    </div>
+                    <div className='containerSides'>
+                        <Typography variant='h3' sx={{fontWeight:'bold', m:2} }> Resumen de Orden </Typography>
+                        <Typography variant='h6' sx={{fontWeight:'bold', m:2, display: 'inline-block'} }> Tipo de orden: </Typography>
+                        <Typography variant='h6' sx={{fontWeight:'normal', m:2, display: 'inline-block'} }> {orderType} </Typography>
+                        <Typography variant='body1' sx={{fontWeight:'normal', m:2} }> 
+                            {
+                                order.items.map (   
+                                    (details, index) => {
+                                        return(
+                                            <Typography variant='body1' sx={{fontWeight:'normal', m:2}} key={index}>
+                                                    {`${details.amount} tamaño ${details.size} de ${details.topping}  ${details.crust}` }
+                                            </Typography>        
+                                        )
+                                    }
+                                )
+                            }
+                        
+                        </Typography>
                     </div>
                 </div>           
 
